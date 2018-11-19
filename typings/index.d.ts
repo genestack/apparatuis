db02d50d346cc405c259c6451365690ca0711b13
Konstantin Vasilev <konstantin.vasiliev@genestack.com>
2018-11-19 14:17:12 +0300

    housekeeping

--- typings/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Allow TS recognize all the ccs modules
- */
-declare module '*.css';
-
-declare module 'postcss-custom-properties';
-declare module 'postcss-import';
-declare module 'autoprefixer';
