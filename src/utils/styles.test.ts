999129d784234f27f01ed9800009a6368dc74b8b
Konstantin Vasilev <konstantin.vasiliev@genestack.com>
2018-12-04 18:52:16 +0300

    ui: fix tests descriptions for styles merger

--- src/utils/styles.test.ts
+++ src/utils/styles.test.ts
@@ -2,51 +2,53 @@ import { mergeClassesProps } from './styles';
 
 const sortClassNames = (str: string) => str.split(' ').sort().join(' ');
 
-describe('class names merger', () => {
-    const moduleClassNames = {
-        'root': '_root',
-        'comp': '_comp',
-    };
+describe('result of calling mergeClassesProps', () => {
+    describe('in common case', () => {
+        const mergeClassesSample = () => mergeClassesProps({
+            someProp: true,
+            className: '__className',
+            classes: {
+                'root': '__root',
+                'comp': '__comp',
+                'any': '__any',
+            },
+        }, {
+            'root': '_root',
+            'comp': '_comp',
+        });
 
-    const props = mergeClassesProps({
-        someProp: true,
-        className: '__className',
-        classes: {
-            'root': '__root',
-            'comp': '__comp',
-            'any': '__any',
-        },
-    }, moduleClassNames);
+        it('should have valid keys', () => {
+            const props = mergeClassesSample();
+            expect(props).toHaveProperty('someProp');
+            expect(props).not.toHaveProperty('className');
+            expect(props).toHaveProperty('classes');
+        });
 
-    it('props should have valid keys', () => {
-        expect(props).toHaveProperty('someProp');
-        expect(props).not.toHaveProperty('className');
-        expect(props).toHaveProperty('classes');
+        it('should have classes property with valid keys', () => {
+            const props = mergeClassesSample();
+            expect(props.classes).toHaveProperty('root');
+            expect(props.classes).toHaveProperty('comp');
+            expect(props.classes).not.toHaveProperty('any');
+        });
+
+        it('should have classes property with valid values', () => {
+            const props = mergeClassesSample();
+            expect(sortClassNames(props.classes.root)).toEqual(sortClassNames('_root __root __className'));
+            expect(sortClassNames(props.classes.comp)).toEqual(sortClassNames('_comp __comp'));
+        });
+
+        it('should have classes.root property which equals main className', () => {
+            const props = mergeClassesSample();
+            expect(props.classes.root).toMatch(/\b__className\b/);
+        });
     });
 
-    it('props.classes should contain valid keys', () => {
-        expect(props.classes).toHaveProperty('root');
-        expect(props.classes).toHaveProperty('comp');
-        expect(props.classes).not.toHaveProperty('any');
-    });
-
-    it('props.classes should have valid values', () => {
-        expect(sortClassNames(props.classes.root)).toEqual(sortClassNames('_root __root __className'));
-        expect(sortClassNames(props.classes.comp)).toEqual(sortClassNames('_comp __comp'));
-    });
-
-    it('root className should contain props.className', () => {
-        expect(props.classes.root).toMatch(/\b__className\b/);
-    });
-});
-
-describe('class names merger should always create root className', () => {
-    it('props.classes.root should not have value without props.className', () => {
+    it('should have classes.root property which equals empty estring when className property is omited', () => {
         const props = mergeClassesProps({}, {});
         expect(props.classes.root).toEqual('');
     });
 
-    it('props.classes.root should not have props.className value', () => {
+    it('should have classes.root property which equals className property when classes property is omited', () => {
         const props = mergeClassesProps({ className: '__root' }, {});
         expect(props.classes.root).toEqual('__root');
     });
