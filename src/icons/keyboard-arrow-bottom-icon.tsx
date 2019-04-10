a37f91dccbb7fc86e172779bf7cad32397cc2e27
Konstantin Vasilev <konstantin.vasiliev@genestack.com>
2019-04-10 17:51:48 +0300

    [FW-113] [UI] Add icons: PlayIcon, ShareIcon, KeyboardArrowBottomIcon

---
+++ src/icons/keyboard-arrow-bottom-icon.tsx
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2011-2019 Genestack Limited
+ * All Rights Reserved
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF GENESTACK LIMITED
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ */
+import * as React from 'react';
+
+import {createIcon} from '../components/icon';
+
+// tslint:disable max-line-length
+
+/**
+ * Keyboard arrow key icon shows user possible transformation direction
+ * after some interaction has applied.
+ */
+export const KeyboardArrowBottomIcon = createIcon(
+    <svg viewBox="0 0 20 20">
+        <path
+            d="M10.354 11.354L13 8.707l.293.293L10 12.293 6.707 9 7 8.707l2.646 2.647.354.353.354-.353z"
+            fill="none"
+            className="gs-svg-icon-stroke"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+        />
+    </svg>
+);
---
+++ src/icons/share-icon.tsx
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2011-2019 Genestack Limited
+ * All Rights Reserved
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF GENESTACK LIMITED
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ */
+import * as React from 'react';
+
+import {createIcon} from '../components/icon';
+
+// tslint:disable max-line-length
+
+/**
+ * Share Icon
+ */
+export const ShareIcon = createIcon(
+    <svg viewBox="0 0 20 20" fill="none">
+        <path className="gs-svg-icon-stroke" d="M9 9l4.5-3M9 11l4.5 3" />
+        <path
+            className="gs-svg-icon-stroke"
+            d="M16.5 6a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zM9.5 10a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0zM15 15.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+    </svg>
+);
