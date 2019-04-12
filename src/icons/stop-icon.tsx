02ee26fae5e92b7c8c2796787727717bab80bbf6
Konstantin Vasilev <konstantin.vasiliev@genestack.com>
2019-04-12 14:57:56 +0300

    [FW-114] [UI] Add ArrowLeftIcon, ArrowRightIcon, MoreIcon, PauseIcon, StopIcon

---
+++ src/icons/stop-icon.tsx
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2011-2019 Genestack Limited
+ * All Rights Reserved
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF GENESTACK LIMITED
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ */
+import * as React from 'react';
+
+import {createIcon} from '../components/icon';
+
+// tslint:disable max-line-length
+
+/**
+ * Stop Icon
+ */
+export const StopIcon = createIcon(
+    <svg viewBox="0 0 20 20">
+        <path d="M5 5h9v10H5z" className="gs-svg-icon-fill" />
+    </svg>
+);
