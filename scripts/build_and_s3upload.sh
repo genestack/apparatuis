aef5474b9786a8bee553e1bfe85923a67475ccbc
Misha Khokhlov <mikhail.khokhlov@genestack.com>
2019-04-15 13:23:36 +0300

    Create script to generate and upload style guide (FW-111) (#378)
    * Create script to generate and upload style guide
    
    * Update env. variable naming
    
    * Trigger CI build
    
    * Trigger CI build
    
    * Replace parameter substitution w/ sed
    
    * Check whether BUILD_BRANCH is empty or not
    
    * Make it possible to filter-out expirable objects
    
    * uikit: rework doc-deploying script
    
    * uikit: don't mangle branch name on uploading to S3
    
    * Add doc
    
    * uikit: fail on undefined variables
    
    * uikit: make builds&upload script executable

---
+++ scripts/build_and_s3upload.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+##################################################################################################################
+# Build and upload ui kit to S3
+#
+# This script builds style guide and uploads the bundle to S3, generating a public URL similar to this:
+# "https://gs-public-resources.s3.amazonaws.com/ui-kit/dev/branch_name/index.html".
+#
+# This script uses AWS CLI (`aws`) and assumes that AWS CLI is already configured.
+#
+# These variables are required in order to build and upload ui kit bundle:
+# * BRANCH — name of the branch you want to build and upload, can be empty
+# * AWS_S3_UIKIT_BUCKET — name of the bucket on AWS S3
+# * AWS_S3_UIKIT_PATH — remote path to a directory where ui kit should be uploaded (e.g. ui-kit)
+# * Other variables required by AWS CLI to operate
+#   (see https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html)
+##################################################################################################################
+
+set -eu
+
+BUNDLE_SUBDIR="styleguide"
+DEV_PATH_INFIX="dev"
+
+if [[ -n "${BRANCH:-}" ]]; then
+    TARGET_PATH="$AWS_S3_UIKIT_PATH/$DEV_PATH_INFIX/$BRANCH"
+else
+    # if $BRANCH isn't defined, default to "master"
+    TARGET_PATH="$AWS_S3_UIKIT_PATH/master"
+fi
+
+TARGET_S3_URL="s3://$AWS_S3_UIKIT_BUCKET/$TARGET_PATH"
+HTML_URL="https://$AWS_S3_UIKIT_BUCKET.s3.amazonaws.com/$TARGET_PATH/index.html"
+
+npm ci
+npm run build
+
+echo "Syncing '$BUNDLE_SUBDIR' to '$TARGET_S3_URL'..."
+
+aws s3 sync "$BUNDLE_SUBDIR" "$TARGET_S3_URL" \
+    --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers \
+    --delete
+
+echo "Synced successfully, see $HTML_URL"
