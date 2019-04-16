96c411a7a53a29928224856191abbf53f4bba1b8
Konstantin Vasilev <konstantin.vasiliev@genestack.com>
2019-04-16 18:36:12 +0300

    [FW-116] [UI] Add FileIcon

---
+++ src/icons/file-icon.tsx
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2011-2019 Genestack Limited
+ * All Rights Reserved
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF GENESTACK LIMITED
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ */
+import * as React from 'react';
+
+import {createIcon} from '../components/icon';
+
+// tslint:disable max-line-length
+
+/**
+ * File icon
+ */
+export const FileIcon = createIcon(
+    <svg viewBox="0 0 20 20">
+        <path
+            d="M4 18V1h9.086L16 3.914V18H4z"
+            fill="none"
+            className="gs-svg-icon-stroke"
+            strokeWidth="2"
+        />
+    </svg>
+);
