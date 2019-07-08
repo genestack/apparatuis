5214a5a440caed6f295cf0e7a7e6b7c878b26f55
Danilian Achmedzianov <danilian.akhmedzianov@genestack.com>
2019-07-08 12:40:17 +0300

    genestack-ui: ODM-1415 Implement preloader component (#476)
    genestack-ui: implement preloader

---
+++ src/components/preloader/preloader.module.css.d.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2011-2019 Genestack Limited
+ * All Rights Reserved
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF GENESTACK LIMITED
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ */
+
+export const width100: string;
+
+export const width75: string;
+
+export const width85: string;
+
+export const preloader: string;
+
+export const backgroundRegular: string;
+
+export const backgroundInverted: string;
