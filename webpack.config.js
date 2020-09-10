e9747746d270ece01ee40e625886f84fe253a84a
Elena Zimina <elena.zimina@genestack.com>
2020-09-10 17:16:36 +0300

    Genestack-ui: update build config to use custom fonts

--- webpack.config.js
+++ webpack.config.js
@@ -117,6 +117,10 @@ module.exports = (env) => {
                         },
                         postCssLoaderParams
                     ]
+                },
+                {
+                    test: /\.(jpe?g|png|gif|svg|woff|woff2|eot|ttf)$/i,
+                    loader: 'file-loader'
                 }
             ]
         },
