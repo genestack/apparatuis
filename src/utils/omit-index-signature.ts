b1260d1e7e993315632244f432922f63358b3f06
Elena Zimina <elena.zimina@genestack.com>
2021-08-09 15:48:36 +0300

    @genestack/ui: bump typescript

--- src/utils/omit-index-signature.ts
+++ src/utils/omit-index-signature.ts
@@ -1,28 +1,10 @@
 /*
- * Copyright (c) 2011-2019 Genestack Limited
+ * Copyright (c) 2011-2021 Genestack Limited
  * All Rights Reserved
  * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF GENESTACK LIMITED
  * The copyright notice above does not evidence any
  * actual or intended publication of such source code.
  */
-/**
- * Returns keys union from the type that have certain keys and index signature.
- *
- * @example
- *
- * interface Foo {
- *   bar?: string;
- *   baz: () => void;
- *   [key: string]: any;
- * }
- *
- * type Bar = KnownKeys<Foo>; // type Bar = 'bar' | 'baz'
- */
-export type KnownKeys<T> = {
-    [K in keyof T]: string extends K ? never : number extends K ? never : K;
-} extends {[_ in keyof T]: infer U}
-    ? U
-    : never;
 
 /**
  * Returns the type without index signature.
@@ -37,4 +19,6 @@ export type KnownKeys<T> = {
  *
  * type Bar = OmitIndexSignature<Foo>; // type Bar = { bar?: string; baz: () => void }
  */
-export type OmitIndexSignature<T extends Record<any, any>> = Pick<T, KnownKeys<T>>;
+export type OmitIndexSignature<T extends Record<any, any>> = {
+    [ K in keyof T as string extends K ? never : number extends K ? never : K ] : T[K]
+};
