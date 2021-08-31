20312b1a1374d5001791afa03b2615904977b8ca
Vladimir Novikov <vladimir.novikov@genestack.com>
2021-08-31 21:24:29 +0300

    ODM-6427: Add NewTabIcon and ExternalLinkIcon icons

---
+++ src/icons/external-link.tsx
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2011-2021 Genestack Limited
+ * All Rights Reserved
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF GENESTACK LIMITED
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ */
+
+import * as React from 'react';
+
+import {createIcon} from '../components/icon';
+
+/**
+ * External link Icon
+ */
+export const ExternalLinkIcon = createIcon(
+    <svg viewBox="0 0 20 20" fill="none">
+        <path
+            className="gs-svg-icon-fill"
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M5 15V5h4v1H6v8h8v-3h1v4H5zm10-6h-1V7l-1 1-3.293 3.293L9 12l-.293-.293-.414-.414L8 11l.707-.707L12 7l1-1h-2V5h4v4z"
+        />
+    </svg>
+);
---
+++ src/icons/new-tab.tsx
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2011-2021 Genestack Limited
+ * All Rights Reserved
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF GENESTACK LIMITED
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ */
+
+import * as React from 'react';
+
+import {createIcon} from '../components/icon';
+
+/**
+ * New Tab Icon
+ */
+export const NewTabIcon = createIcon(
+    <svg viewBox="0 0 20 20" fill="none">
+        <path
+            className="gs-svg-icon-fill"
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M8 5V4h4V3H8c-1 0-1 .5-1 1v8c0 .5.5 1 1 1h8c.5 0 1-.5 1-1V8h-1v4H8V5zM5 15h9v-1h1v1c0 .5-.5 1-1 1H5c-.5 0-1-.5-1-1V7c0-.5.5-1 1-1h1v1H5v8zm12-8h-1V5l-1 1-3.293 3.293L11 10l-.293-.293-.414-.414L10 9l.707-.707L14 5l1-1h-2V3h4v4z"
+        />
+    </svg>
+);
