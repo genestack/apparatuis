84f374e4c60b00430a1c5a8fd12d00bdebbd24b4
Ilya Slukin <ilya.slukin@genestack.com>
2023-06-09 13:16:20 +0200

    new icons added

---
+++ src/icons/minus-icon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {createIcon} from '../components/icon';
+
+/** Minus icon */
+export const MinusIcon = createIcon(
+    <svg viewBox="0 0 20 20" fill="none">
+        <path d="M5 10H15" className="gs-svg-icon-stroke" stroke-linecap="round"/>
+    </svg>
+
+);
