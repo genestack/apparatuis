2c214d4b70a24ec8da23ee202f48d08db189d7f8
Ilya Slukin <ilya.slukin@genestack.com>
2023-06-12 13:40:10 +0200

    prettify, change package version

--- src/icons/minus-icon.tsx
+++ src/icons/minus-icon.tsx
@@ -1,10 +1,17 @@
+/*
+ * Copyright (c) 2011-2019 Genestack Limited
+ * All Rights Reserved
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF GENESTACK LIMITED
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ */
 import React from 'react';
+
 import {createIcon} from '../components/icon';
 
 /** Minus icon */
 export const MinusIcon = createIcon(
     <svg viewBox="0 0 20 20" fill="none">
-        <path d="M5 10H15" className="gs-svg-icon-stroke" stroke-linecap="round"/>
+        <path d="M5 10H15" className="gs-svg-icon-stroke" strokeLinecap="round" />
     </svg>
-
 );
